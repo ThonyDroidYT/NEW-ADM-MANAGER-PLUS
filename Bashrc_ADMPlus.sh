b2533469b16099681c11471639c11e26edf48cf9
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-02-16 22:10:36 -0500

    Update Bashrc_ADMPlus.sh

--- Bashrc_ADMPlus.sh
+++ Bashrc_ADMPlus.sh
@@ -7,11 +7,11 @@ echo "clear" >> .bashrc
 echo 'DATE=$(date +"%d-%m-%y")' >> .bashrc
 echo 'TIME=$(date +"%T")' >> .bashrc
 #echo 'echo -e ""' >> .bashrc
-echo 'echo -e "Nombre del Servidor : $HOSTNAME"' >> .bashrc
-echo 'echo -e "Fecha del Servidor : $DATE"' >> .bashrc
-echo 'echo -e "Hora del Servidor : $TIME"' >> .bashrc
+echo 'echo -e "\e[1;32mNombre del Servidor : \e[1;32m$HOSTNAME\e[0m"' >> .bashrc
+echo 'echo -e "\e[1;33mFecha del Servidor : \e[1;32m$DATE\e[0m"' >> .bashrc
+echo 'echo -e "\e[1;35mHora del Servidor : \e[1;32m$TIME\e[0m"' >> .bashrc
 #echo 'echo -e ""' >> .bashrc
-echo 'echo -e "Bienvenido!"' >> .bashrc
-echo 'echo -e "Teclee vpspack para ver el listado de comandos."' >> .bashrc
+echo 'echo -e "\e[1;33mBienvenido!\e[0m"' >> .bashrc
+echo 'echo -e "\e[1;36mTeclee menu,adm,plus para abrir el script.\e[0m"' >> .bashrc
 echo 'echo -e ""' >> .bashrc
 fi
