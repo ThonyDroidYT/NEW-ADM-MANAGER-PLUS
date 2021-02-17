1094706f6e0083146dd4b5632cfb44cebe6a5961
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-02-16 22:43:56 -0500

    Update Bashrc_ADMPlus.sh

--- Bashrc_ADMPlus.sh
+++ Bashrc_ADMPlus.sh
@@ -6,7 +6,7 @@ else
 cp .bashrc .bashrc.backup
 echo "export PATH=$PATH:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/sbin:/bin:/usr/games;" >> .bashrc
 echo "clear" >> .bashrc
-echo "figlet ADMPlus | lolcat -a -d 5" >> .bashrc
+echo "figlet ADMPlus | lolcat -a -d 10" >> .bashrc
 #echo "figlet -p -f smslant ADMPlus | lolcat" >> .bashrc
 #echo "figlet -p -f smslant < /etc/newadm/nick | lolcat" >> .bashrc
 echo 'DATE=$(date +"%d-%m-%y")' >> .bashrc
