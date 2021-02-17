4d9a0aef3881572d777025d5c85cb06913595e34
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-02-16 21:59:26 -0500

    Update Bashrc_ADMPlus.sh

--- Bashrc_ADMPlus.sh
+++ Bashrc_ADMPlus.sh
@@ -1,3 +1,3 @@
 echo "export PATH=$PATH:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/sbin:/bin:/usr/games;" >> .bashrc
 echo "clear" >> .bashrc
-#echo "figlet ADMPlus | lolcat -a -d 10" >> .bashrc
+#echo "figlet ADMPlus | lolcat -a -d 5" >> .bashrc
