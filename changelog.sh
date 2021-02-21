ad9bee8710d9c299949bb0faebe1439f6067d045
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-02-21 15:09:49 -0500

    Update changelog.sh

--- changelog.sh
+++ changelog.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
-#echo -e "\033[1;41m\033[1;31mLista de Cambios ${name} ${version} ${plain}"
-version="\033[1;32m1.3"
+# Funcoes Globais
+msg () {
+local colors="/etc/new-adm-color"
+if [[ ! -e $colors ]]; then
+COLOR[0]='\033[1;37m' #BRAN='\033[1;37m'
+COLOR[1]='\e[31m' #VERMELHO='\e[31m'
+COLOR[2]='\e[32m' #VERDE='\e[32m'
+COLOR[3]='\e[33m' #AMARELO='\e[33m'
+COLOR[4]='\e[34m' #AZUL='\e[34m'
+COLOR[5]='\e[35m' #MAGENTA='\e[35m'
+COLOR[6]='\033[1;36m' #MAG='\033[1;36m'
+else
+local COL=0
+for number in $(cat $colors); do
+case $number in
+1)COLOR[$COL]='\033[1;37m';;
+2)COLOR[$COL]='\e[31m';;
+3)COLOR[$COL]='\e[32m';;
+4)COLOR[$COL]='\e[33m';;
+5)COLOR[$COL]='\e[34m';;
+6)COLOR[$COL]='\e[35m';;
+7)COLOR[$COL]='\033[1;36m';;
+esac
+let COL++
+done
+fi
+NEGRITO='\e[1m'
+SEMCOR='\e[0m'
+ case $1 in
+  #color new
+  -azul)cor="\033[44m${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -rojo)cor="\033[1;41m${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -gris)cor="\033[1;100m${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -purple)cor="${COLOR[5]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  #fin color
+  -ne)cor="${COLOR[1]}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}";;
+  -ama)cor="${COLOR[3]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -verm)cor="${COLOR[3]}${NEGRITO}[!] ${COLOR[1]}" && echo -e "${cor}${2}${SEMCOR}";;
+  -verm2)cor="${COLOR[1]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -azu)cor="${COLOR[6]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -azule)cor="${COLOR[4]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -verd)cor="${COLOR[2]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  -bra)cor="${COLOR[0]}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
+  "-bar2"|"-bar")cor="${COLOR[4]}=========================================================" && echo -e "${SEMCOR}${cor}${SEMCOR}";;
+ esac
+}
+
+version2="\033[1;32m7.0"
+versión=\e[1;32m$(wget -qO- https://git.io/admplusv)
 name="\033[1;33mMULTISCRIPT-TD"
 plain="\033[0m"
 cyan="\033[1;36m"
@@ -12,16 +59,9 @@ fix="\033[1;31mCorregido:"
 #CAMBIOS
 cambios () {
 clear
-echo -e "\033[1;41m\033[1;36mLista de Cambios ${name} ${version} ${plain}"
-echo -e "${add} ${cyan}VPS-MX ${red}8.3 ${plain}"
-echo -e "${fix} ${cyan}VPS-MX ${red}8.2 ${red}Beta${plain}"
-echo -e "${add} ${cyan}VPS-PACK ${red}5.8 ${plain}"
-echo -e "${add} ${cyan}ADM-MANAGER-ALPHA-MOD ${red}BETA ${plain}"
-}
-#MultiScripts
-multi_scripts () {
-return
-bash <(curl -Ls https://raw.githubusercontent.com/ThonyDroidYT/Herramientas/main/MultiScript-TD.sh)
+msg -bar
+msg -azu "Lista de Cambios ${name} ${version} ${plain}"
+msg -ama "${add} SSLH"
+msg -bar
 }
 cambios
-multi_scripts
