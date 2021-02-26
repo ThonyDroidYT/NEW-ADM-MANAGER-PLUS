b3b314cdbe99c452edb12fd2bba5dbe61fb33f76
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-02-26 15:06:15 -0500

    Update Install_GoLang.sh

--- Files/Install_GoLang.sh
+++ Files/Install_GoLang.sh
@@ -1,18 +1,18 @@
 function goinst(){
-echo -e "A continuacion se instalara el paquete GO Lang"
-echo -e "Continuar?"
+echo -e "\e[1;33mA continuacion se instalara el paquete GO Lang\e[0m"
+echo -e "\e[1;33mContinuar?\e[0m"
 while [[ ${yesno} != @(s|S|y|Y|n|N) ]]; do
 read -p "[S/N]: " yesno
 tput cuu1 && tput dl1
 done
 if [[ ${yesno} = @(s|S|y|Y) ]]; then
-echo -e "Instalando..."
+echo -e "\e[1;32]Instalando...\e[0m"
 cd
 rm -rf /usr/local/go 1> /dev/null 2> /dev/null
 wget https://golang.org/dl/go1.15.linux-amd64.tar.gz 1> /dev/null 2> /dev/null
 tar -C /usr/local -xzf go1.15.linux-amd64.tar.gz
 if cat /root/.bashrc | grep GOROOT; then
-echo -e "Ya existe un GoROOT - Skipping"
+echo -e "\e[1;31mYa existe un GoROOT - Skipping\e[0m"
 else
 sed -i '$a export GOROOT=/usr/local/go' /root/.bashrc
 sed -i '$a export GOPATH=$HOME/go' /root/.bashrc
