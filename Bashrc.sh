70ef9c6066e95831974d3b8a2d79b3a10eeff16b
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-02-26 12:25:24 -0500

    Update Bashrc.sh

--- Bashrc.sh
+++ Bashrc.sh
@@ -12,7 +12,7 @@ echo "figlet ADMPlus | lolcat -a -d 10" >> .bashrc
 #echo "figlet -p -f smslant < /etc/newadm/nick | lolcat" >> .bashrc
 echo 'DATE=$(date +"%d-%m-%y")' >> .bashrc
 echo 'TIME=$(date +"%T")' >> .bashrc
-echo 'echo -e ""' >> .bashrc
+#echo 'echo -e ""' >> .bashrc
 echo 'echo -e "Nombre del Servidor : $HOSTNAME"' >> .bashrc
 echo 'echo -e "Fecha del Servidor : $DATE"' >> .bashrc
 echo 'echo -e "Hora del Servidor : $TIME"' >> .bashrc
