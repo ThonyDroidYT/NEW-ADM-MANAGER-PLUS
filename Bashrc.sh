9ccce24d6532eb6304567c053be40a3b5a25128a
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-02 11:29:01 -0500

    Update Bashrc.sh

--- Bashrc.sh
+++ Bashrc.sh
@@ -5,31 +5,6 @@ if cat /root/.bashrc | grep ADMPlus; then
 exit
 else
 cp .bashrc .bashrc.backup
-echo '
-fun_trans () { 
-local texto
-local retorno
-declare -A texto
-SCPdir="/etc/newadm"
-SCPidioma="${SCPdir}/idioma"
-[[ ! -e ${SCPidioma} ]] && touch ${SCPidioma}
-local LINGUAGE=$(cat ${SCPidioma})
-[[ -z $LINGUAGE ]] && LINGUAGE=es
-[[ $LINGUAGE = "es" ]] && echo "$@" && return
-[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://git.io/trans &> /dev/null
-[[ ! -e /etc/texto-adm ]] && touch /etc/texto-adm
-source /etc/texto-adm
-if [[ -z "$(echo ${texto[$@]})" ]]; then
-#ENGINES=(aspell google deepl bing spell hunspell apertium yandex)
-#NUM="$(($RANDOM%${#ENGINES[@]}))"
-retorno="$(source trans -e bing -b es:${LINGUAGE} "$@"|sed -e 's/[^a-z0-9 -]//ig' 2>/dev/null)"
-echo "texto[$@]='$retorno'"  >> /etc/texto-adm
-echo "$retorno"
-else
-echo "${texto[$@]}"
-fi
-}
-' >> .bashrc
 echo "export PATH=$PATH:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/sbin:/bin:/usr/games;" >> .bashrc
 echo "clear" >> .bashrc
 echo "figlet ADMPlus | lolcat -a -d 08" >> .bashrc
@@ -41,9 +16,9 @@ echo 'TIME=$(date +"%T")' >> .bashrc
 echo 'echo -e "Nombre del Servidor : $HOSTNAME"' >> .bashrc
 echo 'echo -e "Fecha del Servidor : $DATE"' >> .bashrc
 echo 'echo -e "Hora del Servidor : $TIME"' >> .bashrc
+echo 'echo -e "Reseller/Vendedor: $(cat /etc/newadm/message.txt)"' >> .bashrc
 echo 'echo -e ""' >> .bashrc
 #echo 'echo -e "                Bienvenido!               \e[0m"' >> .bashrc
- echo 'echo -e "                $(fun_trans "Bienvenido") \e[0m"' >> .bashrc
 echo 'echo -e "Teclee menu,adm,plus para abrir el script"' >> .bashrc
 echo 'echo -e ""' >> .bashrc
 fi
