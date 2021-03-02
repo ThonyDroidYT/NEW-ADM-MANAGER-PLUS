2079564b92a193d27d3667faf83bfecc6c6bf7b4
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-02 11:30:18 -0500

    Update Bashrc.sh

--- Bashrc.sh
+++ Bashrc.sh
@@ -13,6 +13,7 @@ echo "figlet ADMPlus | lolcat -a -d 08" >> .bashrc
 echo 'DATE=$(date +"%d-%m-%y")' >> .bashrc
 echo 'TIME=$(date +"%T")' >> .bashrc
 #echo 'echo -e ""' >> .bashrc
+#echo 'echo -e "Reseller/Vendedor: $(cat /etc/newadm/message.txt)"' >> .bashrc
 echo 'echo -e "Nombre del Servidor : $HOSTNAME"' >> .bashrc
 echo 'echo -e "Fecha del Servidor : $DATE"' >> .bashrc
 echo 'echo -e "Hora del Servidor : $TIME"' >> .bashrc
