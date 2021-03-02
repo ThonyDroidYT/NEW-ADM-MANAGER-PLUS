272c2925f58e8281b4a311e5b90532b96388ef66
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-02 11:17:24 -0500

    Update Bashrc.sh

--- Bashrc.sh
+++ Bashrc.sh
@@ -6,8 +6,30 @@ exit
 else
 cp .bashrc .bashrc.backup
 echo '
-
-'
+fun_trans () { 
+local texto
+local retorno
+declare -A texto
+SCPdir="/etc/newadm"
+SCPidioma="${SCPdir}/idioma"
+[[ ! -e ${SCPidioma} ]] && touch ${SCPidioma}
+local LINGUAGE=$(cat ${SCPidioma})
+[[ -z $LINGUAGE ]] && LINGUAGE=es
+[[ $LINGUAGE = "es" ]] && echo "$@" && return
+[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://git.io/trans &> /dev/null
+[[ ! -e /etc/texto-adm ]] && touch /etc/texto-adm
+source /etc/texto-adm
+if [[ -z "$(echo ${texto[$@]})" ]]; then
+#ENGINES=(aspell google deepl bing spell hunspell apertium yandex)
+#NUM="$(($RANDOM%${#ENGINES[@]}))"
+retorno="$(source trans -e bing -b es:${LINGUAGE} "$@"|sed -e 's/[^a-z0-9 -]//ig' 2>/dev/null)"
+echo "texto[$@]='$retorno'"  >> /etc/texto-adm
+echo "$retorno"
+else
+echo "${texto[$@]}"
+fi
+}
+' >> .bashrc
 echo "export PATH=$PATH:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/sbin:/bin:/usr/games;" >> .bashrc
 echo "clear" >> .bashrc
 echo "figlet ADMPlus | lolcat -a -d 08" >> .bashrc
