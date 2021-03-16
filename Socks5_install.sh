87e4adae8745297f7cac97473ddb9d7425513942
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-15 23:00:25 -0500

    Update Socks5_install.sh

--- Socks5_install.sh
+++ Socks5_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #BY-THONYDROIDYT
+IP1=$(wget -qO- ipv4.icanhazip.com)
+IP2=$(curl -sSL "https://api.ipify.org")
+[[ $IP1 = $IP2 ]] && IP="${IP2}" || IP="${IP1}"
 socks5 () {
 echo -e "Instalando Socks5 espere..."
 bash <(curl -Ls https://www.admplus.tk/Files/Danted.sh)
@@ -9,7 +12,7 @@ if lsof -Pi :$sockcinco -sTCP:LISTEN -t >/dev/null ; then
 echo "Ya esta en uso ese puerto"
 else
 sed -i '2i internal: 0.0.0.0 port = '"$sockcinco"'' /etc/danted.conf
-sed -i '3i external: '"$ip"'' /etc/danted.conf
+sed -i '3i external: '"$IP"'' /etc/danted.conf
 sleep 5
 echo "Finalizando Instalacion"
 /etc/init.d/danted start
