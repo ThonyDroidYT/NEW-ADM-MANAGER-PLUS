abae9d6958e8c9f451ad2d1d6eee4441bd5b6fa5
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-04-20 19:34:21 -0500

    Create barra2.sh

---
+++ barra2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCPdir="/etc/newadm"
+SCPusr="${SCPdir}/ger-user"
+SCPfrm="/etc/ger-frm"
+SCPinst="/etc/ger-inst"
+sed -i "s;msg -bar2;msg -bar;g" /etc/newadm/menu
+sed -i "s;msg -bar2;msg -bar;g" ${SCPfrm}/*.sh
+sed -i "s;msg -bar2;msg -bar;g" ${SCPinst}/*.sh
+sed -i "s;msg -bar2;msg -bar;g" ${SCPusr}/usercodes
+#ADMbot
+#sed -i "s;$LIN3;$LINE;g" /etc/ger-frm/ADMbot.sh
