2300b64d341962187c265fe1981b343bb5e6d41d
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-07-05 12:16:41 -0500

    Create tema3.sh

---
+++ themes/tema3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -e "\033[1;33mAplicando Tema 4\033[0m"
+rm -rf /etc/new-adm-color
+sleep 1s
+echo "6 5 2 3 4 7 1 1" >> /etc/new-adm-color
+echo "3" >> /etc/newadm/barra
+echo -e "\033[1;32mTema aplicado con éxito\033[0m"
