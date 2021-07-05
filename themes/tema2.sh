ea317f47797d66fd52969dd3b3ba838a553b9dbd
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-07-05 12:15:32 -0500

    Update tema2.sh

--- themes/tema2.sh
+++ themes/tema2.sh
@@ -2,6 +2,6 @@
 echo -e "\033[1;33mAplicando Tema 4\033[0m"
 rm -rf /etc/new-adm-color
 sleep 1s
-echo "7 6 5 4 3 2 1 1" >> /etc/new-adm-color
+echo "6 6 6 6 6 6 1 1" >> /etc/new-adm-color
 echo "3" >> /etc/newadm/barra
 echo -e "\033[1;32mTema aplicado con éxito\033[0m"
