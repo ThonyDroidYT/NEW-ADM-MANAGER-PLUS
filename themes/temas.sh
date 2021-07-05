8b862183a3c226ad96c6373704234647beaebf5f
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-07-05 12:55:10 -0500

    Create temas.sh

---
+++ themes/temas.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo -e "\033[1;33mAplicando Tema $1\033[0m"
+rm -rf /etc/new-adm-color
+rm -rf /etc/newadm/barra
+sleep 1s
+case $1 in
+1)
+color=""
+;;
+2)
+color=""
+;;
+3)
+color=""
+;;
+4)
+color="3 4 5 1 6 7 1"
+;;
+esac
+echo "${color}" >> /etc/new-adm-color
+echo "1" >> /etc/newadm/barra
+echo -e "\033[1;32mTema $1 aplicado con éxito\033[0m"
