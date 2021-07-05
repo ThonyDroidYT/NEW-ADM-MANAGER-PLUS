89302db9533be7a2a426cdc1553894e7c0941916
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-07-05 12:50:05 -0500

    Update tema4.sh

--- themes/tema4.sh
+++ themes/tema4.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo -e "\033[1;33mAplicando Tema 4\033[0m"
 rm -rf /etc/new-adm-color
+rm -rf /etc/newadm/barra
 sleep 1s
 echo "3 4 5 1 6 7 1" >> /etc/new-adm-color
-echo "3" >> /etc/newadm/barra
+echo "1" >> /etc/newadm/barra
 echo -e "\033[1;32mTema aplicado con éxito\033[0m"
